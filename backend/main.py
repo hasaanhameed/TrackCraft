ea3ca8bde9b8e7618aee53c0c79fe1f5bcfa2af6
hasaanhameed <hasaanhameed52@gmail.com>
2025-12-04 22:50:08 +0500

    Changes Reverted

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend import database, models
-from backend.routers import user, authentication, expense
+import database, models
+from routers import user, authentication, expense
 
 app = FastAPI()
 
